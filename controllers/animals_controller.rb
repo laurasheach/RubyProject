f0fb7ea4c9cf14cc7917a9a9f428b59a8135cfc2
Laura Sheach <laura_bubble_git@yahoo.com>
2019-06-02 09:35:35 +0100

    Basic page for showing one animal

--- controllers/animals_controller.rb
+++ controllers/animals_controller.rb
@@ -4,7 +4,16 @@ require( 'pry' )
 require_relative( '../models/animal.rb' )
 also_reload( '../models/*' )
 
+# Show all animals
+
 get '/animals' do
   @animals = Animal.all()
   erb (:"animals/index")
+end
+
+# Show only one animal
+
+get '/animals/:id' do
+  @animal = Animal.find(params[:id])
+  erb(:"animals/show")
 end
